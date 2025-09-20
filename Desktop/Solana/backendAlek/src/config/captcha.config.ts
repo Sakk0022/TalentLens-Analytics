1ea537b0905de7434a63e138c67f516b41414690
Aleksander <154048611+Sakk0022@users.noreply.github.com>
2025-09-20 21:41:45 +0500

    Delete Desktop/Solana/backendAlek directory

--- Desktop/Solana/backendAlek/src/config/captcha.config.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('captcha', () => ({
-  secretKey: process.env.RECAPTCHA_SECRET_KEY || '',
-  minScore: parseFloat(process.env.RECAPTCHA_MIN_SCORE ?? '0.5') || 0.5,
-}));
